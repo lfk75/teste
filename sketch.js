8da1010b91fb059a3c066ddfb15debf9c8065a58
lfk75 <92749457+lfk75@users.noreply.github.com>
2021-10-18 14:56:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var bola;
+function setup() {
+  createCanvas(400,400);
+  bola = createSprite (200,200,15,15)
+  bola.velocityX=2
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
